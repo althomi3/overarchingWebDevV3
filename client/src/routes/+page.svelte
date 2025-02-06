19ffdcfb2dc0c543181b88d1de24d010b066bee5
althomi3 <alischa.thomas@aalto.fi>
2025-02-06 20:41:36 +0100

    sets up web development skeleton

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Message from "$lib/components/Message.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Message />
+
