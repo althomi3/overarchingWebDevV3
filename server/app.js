5aa876a108083e57b60ddfa160afc273bd505c5b
althomi3 <alischa.thomas@aalto.fi>
2025-02-07 16:58:21 +0100

    updats skeleton template

--- server/app.js
+++ server/app.js
@@ -3,16 +3,13 @@ import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import postgres from "postgres";
 
-const app = new Hono();
-const sql = postgres();
-
 app.use("/*", cors());
 app.use("/*", logger());
 
+const app = new Hono();
+const sql = postgres();
+
 app.get("/", (c) => c.json({ message: "Hello world!" }));
-app.get("/todos", async (c) => {
-  const todos = await sql`SELECT * FROM todos`;
-  return c.json(todos);
-});
+
 
 export default app;
