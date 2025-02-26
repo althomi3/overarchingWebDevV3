a07fca2524484236b5fece281014de1ab6335b38
althomi3 <alischa.thomas@aalto.fi>
2025-02-26 17:46:11 +0100

    adds tailwind and skeleton

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import postgres from "postgres";
 
-app.use("/*", cors());
-app.use("/*", logger());
+
 
 const app = new Hono();
 const sql = postgres();
+
+app.use("/*", cors());
+app.use("/*", logger());
 
 app.get("/", (c) => c.json({ message: "Hello world!" }));
 
