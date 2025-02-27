a668e8388e4e01f441a3d4c073a00438f61eb487
althomi3 <alischa.thomas@aalto.fi>
2025-02-27 20:17:18 +0100

    adds styling components

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,32 +1,50 @@
 <script>
    // import Message from "$lib/components/Message.svelte";
-    let count = $state(0);
+    // let count = $state(0);
+    import TodoForm from "$lib/components/TodoForm.svelte";
+    import Todo from "$lib/components/Todo.svelte";
+    import { fade } from "svelte/transition";
+    import { getContext } from "svelte";
+    const toast = getContext("toast");
+
+    let todos = $state([
+    { name: "First todo", done: false },
+    { name: "Second todo", done: true },
+    { name: "Third todo", done: false },
+  ]);
+
+  const remove = (todo) => {
+    if (Math.random() < 0.9) {
+      toast.create({
+        title: "Error",
+        description: "Failed to remove todo",
+        type: "error",
+      });
+			return;
+    }
+
+    todos = todos.filter((t) => t !== todo);
+  };
 
 </script>
 
 <!--<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Message />-->
+<Message />
   
   <h1>Hello world!</h1>
   <p>Count: {count}</p>
-  <button onclick={() => count++}>increment</button>
+  <button onclick={() => count++}>increment</button>-->
+
+ 
+<TodoForm />
+<ul class="space-y-4">
+  {#each todos as todo (todo)}
+    <li transition:fade>
+      <Todo {todo} removeTodo={() => remove(todo)} />
+    </li>
+  {/each}
+</ul>
 
 
-  
-
-  <form class="mx-auto w-full max-w-md space-y-4">
-    <h2 class="h2">Create a todo</h2>
-  
-    <label for="name" class="label">
-      <span class="label-text">Name</span>
-      <input class="input" type="text" id="name" name="name" />
-    </label>
-  
-    <label class="flex items-center space-x-2">
-      <input class="checkbox" type="checkbox" id="completed" name="completed" />
-      <p>Completed</p>
-    </label>
-  
-    <button type="submit" class="w-full btn preset-filled-primary-500">Create</button>
-  </form>
+ 
