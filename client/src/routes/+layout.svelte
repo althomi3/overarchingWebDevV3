63ed52a3b94d733394c53b68accec2afa9081bc4
althomi3 <alischa.thomas@aalto.fi>
2025-03-21 10:23:33 +0100

    adds user state

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,15 +1,25 @@
 <script>
   //export const prerender = true;
   import "../app.css";
+  import { useUserState } from "$lib/states/userState.svelte.js"; // imports user state
   let { children, data } = $props();
   import Header from "$lib/components/layout/Header.svelte";
   import Footer from "$lib/components/layout/Footer.svelte";
   import { ToastProvider } from "@skeletonlabs/skeleton-svelte";
 
+  const userState = useUserState(); // initializes user state
+  if (data.user) {
+    userState.user = data.user; // adds data on user object. the data comes from the hook.server.js which fetches data
+    // from cookies. here, we stored the user name in a cookie which is now stored in the shared user state object
+  }
+
 </script>
 <!--{#if data.user}
   <p>Hello {data.user}!</p>
 {/if}-->
+{#if data.user}
+  <p>Hello {data.user}!</p>
+{/if}
 
 <ToastProvider>
   <div class="flex flex-col h-full">
